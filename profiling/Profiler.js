356f5f50f6fc80dded56e1d8ede0d2edfc859e43
malte.ubl <malte.ubl@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2008-09-29 20:37:53 +0000

    Very basic start of a JS profiler (I have to commit this because I'll change jobs and I'll return my MBP tomorrow)
    git-svn-id: http://joose-js.googlecode.com/svn/trunk@253 7be8dbaf-bf49-0410-bd10-bfef82906ff6

---
+++ profiling/Profiler.js
@@ -0,0 +1,34 @@
+
+
+Profiler = function (job, desc) {
+	this.job         = job;
+	this.description = desc;
+	this.start       = new Date()	
+	this.end         = new Date()
+	this.iterations  = 0;
+}
+
+Profiler.prototype = {
+	run: function (count) {
+		this.iterations = count;
+		var job         = this.job;
+		this.start      = new Date();
+		for(var i = 0; i < count; ++i) {
+			job()
+		}
+		this.end   = new Date();
+		return this;
+	},
+	
+	duration: function () {
+		return this.end.getTime() - this.start.getTime();
+	},
+	
+	report: function () {
+		var duration = this.duration() / 1000;
+		document.write("" +this.iterations + " iterations of " + this.description + " took " + duration + " seconds.<br>\n")
+		return this;
+	}
+}
+
+
