5be8566865fc4eb93a32b4b7d92128bb59b56750
jeremy@marzhillstudios.com <jeremy@marzhillstudios.com@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2008-11-04 04:29:53 +0000

    release script has pod and proper commandline arguments
    git-svn-id: http://joose-js.googlecode.com/svn/trunk@327 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- build/release.pl
+++ build/release.pl
@@ -4,21 +4,73 @@ use strict;
 use FindBin qw{$Bin};
 use File::Find;
 use File::Copy;
+use Getopt::Long;
+use Pod::Usage;
 
-# prepares a joose release archive
-# containing
-# - lib
-# - joose.js // all of lib in one file
-# - joose.js.gz // gzipped
-# - joose.mini.js // 
-# - joose.mini.js.gz // gzipped
-# - examples
-# - ext
-# - tests
-# - README, INSTAL, LICENCE
+=head1 NAME
+    
+    release.pl - release script for the joose javascript library
 
-my $release_dir = shift || die "need release dir";
-my $version     = shift || die "need version";
+=head1 SYNOPSIS
+
+    release.pl -d <directory> -v <version> [-h]
+
+=head1 OPTIONS
+
+=over 2
+
+=item C<-d> direcgtory where script will build the joose release
+
+=item C<-v> the version of the joose release the script is building
+
+=item C<-h> display this help
+
+=back
+
+all options except C<-h> are required:
+
+
+=head1 DESCRIPTION
+
+prepares a joose release archive containing:
+
+=over 2
+
+=item - lib
+
+=item - joose.js // all of lib in one file
+
+=item - joose.js.gz // gzipped
+
+=item - joose.mini.js // 
+
+=item - joose.mini.js.gz // gzipped
+
+=item - examples
+
+=item - ext
+
+=item - tests
+
+=item - README, INSTAL, LICENCE
+
+=back
+
+=cut
+
+my ($release_dir, $version, $help);
+
+my $result = GetOptions(
+    "d=s" => \$release_dir,
+    "v=s" => \$version,
+    "h"   => \$help
+    );
+
+if ($help) {
+    pod2usage( -verbose => 2, -exitval => 0);
+} elsif (!$release_dir || !$version) {
+    pod2usage( -verbose => 2, -exitval => 99);
+}
 
 my $joose_dir   = "joose-$version";
 my $compile_dir = "$release_dir/$joose_dir";
