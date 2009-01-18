e18c685d6c24262599ec6eba10191c883da7ed3e
Jeremy@marzhillstudios.com <Jeremy@marzhillstudios.com@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-01-18 04:52:00 +0000

    method working in a class with tests to prove it
    git-svn-id: http://joose-js.googlecode.com/svn/trunk@451 7be8dbaf-bf49-0410-bd10-bfef82906ff6

---
+++ tests/22_multi_method.t.js
@@ -0,0 +1,76 @@
+(function () {
+var testobj = new Test.TAP.Class();
+testobj.plan(7)
+
+testobj.testMultiMethod = function() {
+    var t = this;
+    t.diag("Sanity");
+    t.ok(Joose,   "Joose is here");
+    t.ok(JooseX.MultiMethod,   "We have the MultiMethod class");
+
+    Class("MultiMethodTestClass", {
+        methods: {
+            test: function () { return "foo"  }
+        }
+    });
+    
+    var method = new JooseX.MultiMethod("multi", function () {}, {});
+    var patterns = [
+        {
+            signature : [],
+            fun       : function() {
+                return "foo";
+            }
+        },
+        {
+            signature : [TYPE.Str],
+            fun       : function() {
+                return "fooString";
+            }
+        },
+        {
+            signature : [Array],
+            fun       : function() {
+                return "fooArray";
+            }
+        },
+        {
+            signature : [TYPE.Str, TYPE.Func],
+            fun       : function() {
+                return "fooStringFunc";
+            }
+        },
+        {
+            signature : [Object],
+            fun       : function() {
+                return "fooObject";
+            }
+        }
+    ];
+    method.setPatterns(patterns);
+    MultiMethodTestClass.meta.addMethodObject(method);
+    
+    var o = new MultiMethodTestClass();
+    
+    t.ok(o.meta.can("multi"), "Method is there");
+    t.ok(typeof o.multi === "function", "There is actually a function in the spot");
+    
+    t.lives_ok(function () {
+        result = o.multi();
+    }, "calling with correct argument signature lives")
+   
+    t.ok(result === "foo", "result from method call is correct")
+    
+    // exceptions
+    t.throws_ok(function () {
+        o.multi(1,2,3)
+    }, /multi-method type method call with no matching signature/, 
+        "Calling with incorrect signature throws exception")
+    
+    // builder syntax
+    //t.diag("Testing builder syntax for typed methods")
+    
+};
+
+return testobj;
+})()
