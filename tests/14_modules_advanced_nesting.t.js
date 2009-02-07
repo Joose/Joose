2a25149c384221b28f65e41625a5ae9d6ae2496a
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-02-07 21:44:22 +0000

    - updated advanced nesting test
    git-svn-id: http://joose-js.googlecode.com/svn/branches/dependencies@545 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- tests/14_modules_advanced_nesting.t.js
+++ tests/14_modules_advanced_nesting.t.js
@@ -18,39 +18,39 @@ t.testModuleClass = function() {
             self.ok(StressTest.Nested, "Something in the nested module spot");
             self.ok(StressTest.Nested.meta.meta.isa(Joose.Kernel.Namespace), "And its a Joose.Kernel.Namespace");
             
-            Module("Level1", {
+            Module("Level1_1", {
                 use : 'StressTest.Test050',
                 
                 body : function () {
-                    Class("Level2", {
+                    Class("Level2_1", {
                         methods : {
                             three : function () { return 3 }
                         },
                         body : function (){
-                            self.ok(new StressTest.Nested.Level1.Level2().three() == 3, "StressTest.Nested.Level1.Level2 works correctly #1");
+                            self.ok(new StressTest.Nested.Level1_1.Level2_1().three() == 3, "StressTest.Nested.Level1_1.Level2_1 works correctly #1");
                         }
                     })
-                    self.ok(StressTest.Nested.Level1.Level2, "Something in the nested module spot, at level 2, #1");
+                    self.ok(StressTest.Nested.Level1_1.Level2_1, "Something in the nested module spot, at level 2, #1");
                 }
             });
-            self.ok(StressTest.Nested.Level1, "Something in the nested module spot, at level 1, #1");
+            self.ok(StressTest.Nested.Level1_1, "Something in the nested module spot, at level 1, #1");
             
-            Module("Level1", {
+            Module("Level1_2", {
                 use : 'StressTest.Test075',
                 
                 body : function () {
-                    Class("Level2", {
+                    Class("Level2_2", {
                         methods : {
                             four : function () { return 4 }
                         },
                         body : function (){
-                            self.ok(new StressTest.Nested.Level1.Level2().four() == 4, "StressTest.Nested.Level1.Level2 works correctly #2");
+                            self.ok(new StressTest.Nested.Level1_2.Level2_2().four() == 4, "StressTest.Nested.Level1_2.Level2_2 works correctly #2");
                         }
                     })
-                    self.ok(StressTest.Nested.Level1.Level2, "Something in the nested module spot, at level 2, #2");
+                    self.ok(StressTest.Nested.Level1_2.Level2_2, "Something in the nested module spot, at level 2, #2");
                 }
             });
-            self.ok(StressTest.Nested.Level1, "Something in the nested module spot, at level 1, #2");
+            self.ok(StressTest.Nested.Level1_2, "Something in the nested module spot, at level 1, #2");
         }
     })
     
