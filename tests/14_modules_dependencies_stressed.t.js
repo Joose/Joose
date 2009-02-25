31beb1d7112aac882fc463ae88c02429ab71b83c
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-02-25 20:40:02 +0000

    - added SKIP sections to test w/o Depended role included
    git-svn-id: http://joose-js.googlecode.com/svn/branches/temp@624 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- tests/14_modules_dependencies_stressed.t.js
+++ tests/14_modules_dependencies_stressed.t.js
@@ -7,47 +7,50 @@ var thistop = Test.prototype.top()
 
 t.testModuleClass = function() {
     var self = this;
+    
+    self.skip(typeof Joose.Kernel.MetaClass.Depended != 'function', "Depended Role not included", 319, function(){
 
-    Module("StressTest");
-    self.ok(StressTest, "Root module created");
-    __global__.doubleDeclarations = false;
-    __global__.unSatisfiedDeps = false;
-    //==================================================================================================================================================================================
-    self.diag("Stress testing of dependencies loading");
-
-    var start      = new Date();
-    Module("StressTest", {
-        use : 'StressTest.Test001',
-        
-        //body is executing after the all dependencies are satisfied
-        body : function(){
-            var end   = new Date();
+        Module("StressTest");
+        self.ok(StressTest, "Root module created");
+        __global__.doubleDeclarations = false;
+        __global__.unSatisfiedDeps = false;
+        //==================================================================================================================================================================================
+        self.diag("Stress testing of dependencies loading");
+    
+        var start      = new Date();
+        Module("StressTest", {
+            use : 'StressTest.Test001',
             
-            self.diag("Duration = " + (end.getTime() - start.getTime()) / 1000);
-            
-            self.ok(!__global__.doubleDeclarations, "Stress testing passed without redeclarations");
-            self.ok(!__global__.unSatisfiedDeps, "Stress testing passed with all dependencies satisfied");
-            
-            self.ok(StressTest.Test001, "StressTest.Test001 module created");
-            self.ok(StressTest.Test001.meta.meta.isa(Joose.Class), "StressTest.Test001 class created");
-            self.ok(StressTest.Test001.meta.can('result'), "StressTest.Test001 has method 'result'");
-            self.ok(new StressTest.Test001().result() == 1, 'StressTest.Test001 can be instantiated');
-            
-            for (var i = 2; i <= 100; i++) {
-                var class_name = new String(i).split('');
-                while (class_name.length < 3) class_name.unshift('0');
-                class_name = 'Test' + class_name.join('');
+            //body is executing after the all dependencies are satisfied
+            body : function(){
+                var end   = new Date();
                 
-                if (typeof StressTest[class_name] == 'function') {
-                    self.ok(StressTest[class_name], "StressTest." + class_name + " module created");
-                    self.ok(StressTest[class_name].meta.meta.isa(Joose.Class), "StressTest." + class_name + " class created");
-                    self.ok(StressTest[class_name].meta.can('result'), "StressTest." + class_name + " has method 'result'");
-                    self.ok(new (StressTest[class_name])().result() == i, "StressTest." + class_name + " can be instantiated");
+                self.diag("Duration = " + (end.getTime() - start.getTime()) / 1000);
+                
+                self.ok(!__global__.doubleDeclarations, "Stress testing passed without redeclarations");
+                self.ok(!__global__.unSatisfiedDeps, "Stress testing passed with all dependencies satisfied");
+                
+                self.ok(StressTest.Test001, "StressTest.Test001 module created");
+                self.ok(StressTest.Test001.meta.meta.isa(Joose.Class), "StressTest.Test001 class created");
+                self.ok(StressTest.Test001.meta.can('result'), "StressTest.Test001 has method 'result'");
+                self.ok(new StressTest.Test001().result() == 1, 'StressTest.Test001 can be instantiated');
+                
+                for (var i = 2; i <= 100; i++) {
+                    var class_name = new String(i).split('');
+                    while (class_name.length < 3) class_name.unshift('0');
+                    class_name = 'Test' + class_name.join('');
+                    
+                    if (typeof StressTest[class_name] == 'function') {
+                        self.ok(StressTest[class_name], "StressTest." + class_name + " module created");
+                        self.ok(StressTest[class_name].meta.meta.isa(Joose.Class), "StressTest." + class_name + " class created");
+                        self.ok(StressTest[class_name].meta.can('result'), "StressTest." + class_name + " has method 'result'");
+                        self.ok(new (StressTest[class_name])().result() == i, "StressTest." + class_name + " can be instantiated");
+                    }
                 }
             }
-        }
+        });
+        
     });
-    
 }
 
 return t;
--- tests/14_modules_versioning.t.js
+++ tests/14_modules_versioning.t.js
@@ -11,35 +11,38 @@ t.testModuleClass = function() {
     //==================================================================================================================================================================================
     self.diag("Versioning");
     
-    Module("StressTest.Versioning", {
-        use : { Module : 'StressTest.Test035', version : 0.05 },
-        body : function () {
-            
-            self.ok(StressTest.Versioning, "Something in the Versioning module spot");
-            self.ok(true, "StressTest.Test035 has higher version than required");
-            
-            //TODO Global exceptions intercepting
-            //self.throws_ok(function(){
-//                Module("Level1_1", {
-//                    use : { Module : 'StressTest.Test050', version : 1.01 },
-//                    
-//                    body : function () {
-//                        Class("Level2_1", {
-//                            methods : {
-//                                three : function () { return 3 }
-//                            },
-//                            body : function (){
-//                                self.ok(new StressTest.Nested.Level1_1.Level2_1().three() == 3, "StressTest.Nested.Level1_1.Level2_1 works correctly #1");
-//                            }
-//                        })
-//                        self.ok(StressTest.Nested.Level1_1.Level2_1, "Something in the nested module spot, at level 2, #1");
-//                    }
-//                });
-            //TODO Global exceptions intercepting
-            //}, 'Loaded dependency StressTest.Test050 has lower version [0.1] than required [1.01]', 'StressTest.Test050 has lower version than required');
-//            self.ok(!StressTest.Versioning.Level1_1.meta.meta.isa(Joose.Class), "There is no class in the StressTest.Versioning.Level1_1 module spot");
-        }
-    })
+    self.skip(typeof Joose.Kernel.MetaClass.Depended != 'function', "Depended Role not included", 2, function(){
+    
+        Module("StressTest.Versioning", {
+            use : { Module : 'StressTest.Test035', version : 0.05 },
+            body : function () {
+                
+                self.ok(StressTest.Versioning, "Something in the Versioning module spot");
+                self.ok(true, "StressTest.Test035 has higher version than required");
+                
+                //TODO Global exceptions intercepting
+                //self.throws_ok(function(){
+    //                Module("Level1_1", {
+    //                    use : { Module : 'StressTest.Test050', version : 1.01 },
+    //                    
+    //                    body : function () {
+    //                        Class("Level2_1", {
+    //                            methods : {
+    //                                three : function () { return 3 }
+    //                            },
+    //                            body : function (){
+    //                                self.ok(new StressTest.Nested.Level1_1.Level2_1().three() == 3, "StressTest.Nested.Level1_1.Level2_1 works correctly #1");
+    //                            }
+    //                        })
+    //                        self.ok(StressTest.Nested.Level1_1.Level2_1, "Something in the nested module spot, at level 2, #1");
+    //                    }
+    //                });
+                //TODO Global exceptions intercepting
+                //}, 'Loaded dependency StressTest.Test050 has lower version [0.1] than required [1.01]', 'StressTest.Test050 has lower version than required');
+    //            self.ok(!StressTest.Versioning.Level1_1.meta.meta.isa(Joose.Class), "There is no class in the StressTest.Versioning.Level1_1 module spot");
+            }
+        })
+    });
     
 }
 
