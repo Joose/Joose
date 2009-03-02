705b1da5cd333df066e36b89298332afa1569d26
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-03-02 13:49:42 +0000

    - small fixes - added builder extension tests
    git-svn-id: http://joose-js.googlecode.com/svn/branches/mutability@640 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- tests/00_02_managedmeta.t.js
+++ tests/00_02_managedmeta.t.js
@@ -10,7 +10,7 @@ testobj.testSanity = function() {
     
     
     //==================================================================================================================================================================================
-    this.diag("Creation");
+    this.diag("Creation & managed extending (building)");
     
     var TestClass = new Joose.Managed.Meta('TestClass', null, null, {
         have : {
@@ -24,15 +24,53 @@ testobj.testSanity = function() {
     
     this.ok(typeof TestClass == 'function', "TestClass was created");
     
-//    debugger;
     this.ok(TestClass.meta.hasAttribute('res'), "TestClass has 'res' attribute"); 
     this.ok(TestClass.meta.hasMethod('result'), "TestClass has 'result' method");
+
+    this.ok(TestClass.meta.hasOwnAttribute('res'), "TestClass has own 'res' attribute"); 
+    this.ok(TestClass.meta.hasOwnMethod('result'), "TestClass has own 'result' method");
+    
+    this.ok(TestClass.meta.hasMethod('initialize'), "TestClass has 'initialize' method");
+    this.ok(TestClass.meta.hasMethod('SUPER'), "TestClass has 'SUPER' method");
+    
+    this.ok(!TestClass.meta.hasOwnMethod('initialize'), "TestClass doesnt have own 'initialize' method");
+    this.ok(!TestClass.meta.hasOwnMethod('SUPER'), "TestClass doesnt have own 'SUPER' method");
     
     var testClass = new TestClass();
     
     this.ok(testClass, "TestClass was instantiated");
     this.ok(testClass.res, "Attribute was correctly installed");
     this.is(testClass.result(), '1', "Method was correctly installed");
+    
+    
+    //==================================================================================================================================================================================
+    this.diag("Extending of builder");
+    
+    var TestClass1 = new Joose.Managed.Meta('TestClass1', null, null, {
+        builder : {
+            testHandler : function(meta, props){
+                var name = props.name;
+                var value = props.value;
+                
+                meta.addMethod(name, function(){
+                    return value;
+                });
+            }
+        },
+        
+        testHandler : {
+            name : 'result',
+            value : '1'
+        }
+        
+    }).c;
+    
+    this.ok(TestClass1.meta.hasMethod('result'), "Method was added via extended builder");
+    
+    var testClass1 = new TestClass1();
+    
+    this.is(testClass1.result(), '1', "... and it works correctly");
+    
 };
 
 return testobj;
