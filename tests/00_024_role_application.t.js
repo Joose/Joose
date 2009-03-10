84d4adc4eb68f76501b2624db46c91bbb34a1037
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-03-10 14:36:07 +0000

    - extended 00_024_role_application.t.js - fixed bug in role removing
    git-svn-id: http://joose-js.googlecode.com/svn/branches/mutability@676 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- tests/00_024_role_application.t.js
+++ tests/00_024_role_application.t.js
@@ -18,8 +18,8 @@ testobj.testSanity = function() {
         },
         
         methods : {
-            walk : function (where) { return this.walking = true },
-            stop : function () { return this.walking = false }
+            walk : function (where) { this.walking = true },
+            stop : function () { this.walking = false }
         }
     }).c;
     
@@ -34,13 +34,13 @@ testobj.testSanity = function() {
         },
         
         methods : {
-            eat : function (food) { return this.eating = true },
-            stop : function () { return this.eating = false }
+            eat : function (food) { this.eating = true },
+            stop : function () { this.eating = false }
         }
     }).c;
     
     
-    var Dog = new Joose.Managed.Class('Dog', {
+    var Creature = new Joose.Managed.Class('Creature', {
         does : [{
             role : Walk,
             alias : {
@@ -57,210 +57,194 @@ testobj.testSanity = function() {
     }).c;
     
 
-    this.ok(Dog.meta.hasAttribute('walking') && Dog.meta.getAttribute('walking').value == false, "Dog has correct attribute 'walking'");
-    this.ok(Dog.meta.hasAttribute('eating') && Dog.meta.getAttribute('eating').value == false, "Dog has correct attribute 'eating'");
-    this.ok(Dog.meta.hasMethod('walk'), 'Dog has method walk');
-    this.ok(Dog.meta.hasMethod('eat'), 'Dog has method eat');
-    this.ok(Dog.meta.hasMethod('stopWalk'), 'Dog has method stopWalk');
-    this.ok(Dog.meta.hasMethod('stopEat'), 'Dog has method stopEat');
-    this.ok(!Dog.meta.hasMethod('stop'), 'Dog hasnt method stop');
+    this.ok(Creature.meta.hasAttribute('walking') && Creature.meta.getAttribute('walking').value == false, "Creature has correct attribute 'walking'");
+    this.ok(Creature.meta.hasAttribute('eating') && Creature.meta.getAttribute('eating').value == false, "Creature has correct attribute 'eating'");
+    this.ok(Creature.meta.hasMethod('walk'), 'Creature has method walk');
+    this.ok(Creature.meta.hasMethod('eat'), 'Creature has method eat');
+    this.ok(Creature.meta.hasMethod('stopWalk'), 'Creature has method stopWalk');
+    this.ok(Creature.meta.hasMethod('stopEat'), 'Creature has method stopEat');
+    this.ok(!Creature.meta.hasMethod('stop'), 'Creature hasnt method stop');
+    
+    var creature = new Creature();
+    
+    creature.walk('there');
+    this.ok(creature.walking, 'Creature is walking');
+    creature.stopWalk();
+    this.ok(!creature.walking, 'Creature is not walking');
+    
+
+    //==================================================================================================================================================================================
+    this.diag("Cannibal creature");
+    
+    var Cannibalism = new Joose.Managed.Role('Cannibalism', { 
+        override : {
+            eat : function (food) { 
+                if (food.constructor == this.constructor) this.SUPER(food); 
+            }
+        }
+    }).c;
     
     
-//    var Human = new Joose.Managed.Class('Human', {
-//        does : [ Walk, Eat ],
-//        
-//        override : {
-//            
-//        }
-//    }).c;
+    var Cannibal = new Joose.Managed.Class('Cannibal', {
+        isa : Creature,
+        
+        does : [ Cannibalism ]
+    }).c;    
     
     
+    var cannibal1 = new Cannibal();
+    var cannibal2 = new Cannibal();
+    var creature = new Creature();
     
-//    //==================================================================================================================================================================================
-//    this.diag("Basic composition");
-//    
-//    var B = new Joose.Managed.Role('B', { 
-//        have : {
-//            B1 : 'B1',
-//            A1 : 'B-A1'
-//        },
-//        
-//        methods : {
-//            B2 : function () { return 'B2' }
-//        },
-//        
-//        does : [ A ]
-//    }).c;
-//    
-//    this.ok(B.meta.hasMethod('A2') && B.meta.getMethod('A2') == A.meta.getMethod('A2'), 'method A2 was composed from A #1');
-//    this.is(B.meta.getAttribute('A1').value, 'B-A1', "A1 property of B don't changed");
-//    
-//    
-//    var C = new Joose.Managed.Role('C', {
-//        have : {
-//            C1 : 'C1'
-//        },
-//        
-//        does : [ B ]
-//    }).c;
-//    
-//    //==================================================================================================================================================================================
-//    this.diag("Composition with conflicting flattening");
-//    
-//    var E = new Joose.Managed.Role('E', {
-//        have : {
-//            E1 : 'E1'
-//        },
-//        
-//        methods : {
-//            E2 : function () { return 'E2' }
-//        },
-//        
-//        does : [ A, B ]
-//    }).c;
-//    
-//    this.ok(E.meta.hasAttribute('B1') && E.meta.getAttribute('B1') == B.meta.getAttribute('B1'), 'E received B1');
-//    
-//    this.ok(E.meta.hasAttribute('A1'), 'E received A1');
-//    this.ok(E.meta.getAttribute('A1') instanceof Joose.Managed.Property.ConflictMarker, 'A1 is a conflict marker actually');
-//    
-//    this.ok(E.meta.hasMethod('A2') && E.meta.getMethod('A2') == A.meta.getMethod('A2'), 'E received A2');
-//    this.ok(E.meta.hasMethod('B2') && E.meta.getMethod('B2') == B.meta.getMethod('B2'), 'E received B2');
-//    
-//
-//
-//    //==================================================================================================================================================================================
-//    this.diag("Composition with conflicting flattening #2");
-//    
-//    var D = new Joose.Managed.Role('D', {
-//        have : {
-//            D1 : 'D1'
-//        },
-//        
-//        does : [ B, E ]
-//    }).c;
-//    
-//    
-//    this.ok(D.meta.hasAttribute('A1') && D.meta.getAttribute('A1') instanceof Joose.Managed.Property.ConflictMarker, 'A1 is still a conflict marker');
-//    
-//    this.ok(D.meta.hasMethod('A2') && D.meta.getMethod('A2') == A.meta.getMethod('A2'), 'D received A2');
-//    this.ok(D.meta.hasMethod('B2') && D.meta.getMethod('B2') == B.meta.getMethod('B2'), 'D received B2');
-//    this.ok(D.meta.hasMethod('E2') && D.meta.getMethod('E2') == E.meta.getMethod('E2'), 'D received E2');
-//    
-//    
-//    //==================================================================================================================================================================================
-//    this.diag("Composition with conflict resolution");
-//    
-//    var F = new Joose.Managed.Role('F', {
-//        have : {
-//            F1 : 'F1',
-//            A1 : 'F-A1'
-//        },
-//        
-//        methods : {
-//            F2 : function () { return 'F2' }
-//        },
-//        
-//        does : [ C, D, E ]
-//    }).c;
-//    
-//    
-//    this.ok(F.meta.hasMethod('A2') && F.meta.getMethod('A2') == A.meta.getMethod('A2'), 'F received A2');
-//    this.ok(F.meta.hasAttribute('D1') && F.meta.getAttribute('D1') == D.meta.getAttribute('D1'), 'F received D1');
-//    this.ok(F.meta.hasMethod('E2') && F.meta.getMethod('E2') == E.meta.getMethod('E2'), 'F received E2');
-//    
-//    this.ok(F.meta.hasAttribute('A1'), 'F got A1');
-//    this.ok(!(F.meta.getAttribute('A1') instanceof Joose.Managed.Property.ConflictMarker), 'A1 is not a conflict marker');
-//    this.is(F.meta.getAttribute('A1').value, 'F-A1', 'Conflict was resolved');
-//
-//    
-//    //==================================================================================================================================================================================
-//    this.diag("Aliasing & exclusion");
-//    
-//    var E1 = new Joose.Managed.Role('E1', {
-//        have : {
-//            E11 : 'E11'
-//        },
-//        
-//        does : [{
-//            role : A,
-//            alias : {
-//                A1 : 'A1_from_A'
-//            },
-//            exclude : [ 'A2' ]
-//        },{
-//            role : B,
-//            alias : {
-//                A1 : 'A1_from_B'
-//            },
-//            exclude : [ 'A1', 'B1' ]
-//        }]
-//    }).c;
-//    
-//    this.ok(!E1.meta.hasAttribute('B1'), "F don't received B1");
-//    
-//    this.ok(E1.meta.hasMethod('A2') && E1.meta.getMethod('A2') == B.meta.getMethod('A2'), "F still received A2 from B");
-//    this.ok(E1.meta.hasMethod('B2') && E1.meta.getMethod('B2') == E.meta.getMethod('B2'), 'E1 received B2');
-//    
-//    this.ok(E1.meta.hasAttribute('A1'), "F now received A1 from A without conflict");
-//    this.ok(!(E1.meta.getAttribute('A1') instanceof Joose.Managed.Property.ConflictMarker), 'A1 is not a conflict marker');
-//    this.ok(E1.meta.getAttribute('A1') == A.meta.getAttribute('A1'), "F now received A1 from A");
-//    
-//    this.ok(E1.meta.hasAttribute('A1_from_A'), 'F received A1_from_A #1');
-//    this.ok(E1.meta.getAttribute('A1_from_A').value == 'A1', 'F received A1_from_A #2');
-//    
-//    this.ok(E1.meta.hasAttribute('A1_from_B'), 'F received A1_from_B #1');
-//    this.ok(E1.meta.getAttribute('A1_from_B').value == 'B-A1', 'F received A1_from_B #2');
-//    
-//    
-//    //==================================================================================================================================================================================
-//    this.diag("Mutability");
-//    
-//    A.meta.extend({
-//        methods : {
-//            A3 : function () { return 'A3' }
-//        }        
-//    });
-//    
-//    this.ok(B.meta.hasMethod('A3') && B.meta.getMethod('A3') == A.meta.getMethod('A3'), 'B received A3 property via mutation');    
-//    this.ok(C.meta.hasMethod('A3') && C.meta.getMethod('A3') == A.meta.getMethod('A3'), 'C received A3 property via mutation');
-//    this.ok(D.meta.hasMethod('A3') && D.meta.getMethod('A3') == A.meta.getMethod('A3'), 'D received A3 property via mutation');
-//    this.ok(E.meta.hasMethod('A3') && E.meta.getMethod('A3') == A.meta.getMethod('A3'), 'E received A3 property via mutation');
-//    this.ok(F.meta.hasMethod('A3') && F.meta.getMethod('A3') == A.meta.getMethod('A3'), 'F received A3 property via mutation');
-//    this.ok(E1.meta.hasMethod('A3') && E1.meta.getMethod('A3') == A.meta.getMethod('A3'), 'E1 received A3 property via mutation');
-//    
-//    
-//    A.meta.extend({
-//        removeMethods : [ 'A3' ]
-//    });    
-//    
-//    this.ok(!B.meta.hasMethod('A3'), 'B lost A3 property via mutation');    
-//    this.ok(!C.meta.hasMethod('A3'), 'C lost A3 property via mutation');
-//    this.ok(!D.meta.hasMethod('A3'), 'D lost A3 property via mutation');
-//    this.ok(!E.meta.hasMethod('A3'), 'E lost A3 property via mutation');
-//    this.ok(!F.meta.hasMethod('A3'), 'F lost A3 property via mutation');
-//    this.ok(!E1.meta.hasMethod('A3'), 'E1 lost A3 property via mutation');
-//    
-//    
-//    F.meta.extend({
-//        have : {
-//            C1 : 'F-C1',
-//            D1 : 'F-D1'
-//        },
-//        
-//        havenot : [ 'A1' ]
-//    });
-//
-//    this.ok(F.meta.hasAttribute('C1') && F.meta.getAttribute('C1').value == 'F-C1', 'F have override C1 property during mutation');
-//    this.ok(F.meta.hasAttribute('D1') && F.meta.getAttribute('D1').value == 'F-D1', 'F have override D1 property during mutation');
-//    this.ok(F.meta.getAttribute('A1') instanceof Joose.Managed.Property.ConflictMarker, 'A1 of F is now a conflict marker');
-//    
-//    B.meta.extend({
-//        havenot : [ 'A1' ]
-//    });
-//    
-//    this.ok(!(F.meta.getAttribute('A1') instanceof Joose.Managed.Property.ConflictMarker), 'A1 of F is now not a conflict marker');
-//    this.ok(F.meta.getAttribute('A1') == A.meta.getAttribute('A1'), 'A1 of F is now obtained from A');
+    cannibal1.eat(creature);
+    this.ok(!cannibal1.eating, "Cannibal eats only creatures from his species #1 + method modifier from Role works");
+
+    cannibal1.eat(cannibal2);
+    this.ok(cannibal1.eating, "Cannibal eats only creatures from his species #2");
+    cannibal1.stopEat();
+    this.ok(!cannibal1.eating, "Cannibal1 ate cannibal2 )");
+    
+    
+    //==================================================================================================================================================================================
+    this.diag("Human");
+    
+    var Plant = new Joose.Managed.Class('Plant', {
+        methods : {
+            grow : function () {}
+        }
+    }).c;
+    
+    var plant = new Plant();
+    
+    
+    var Drive = new Joose.Managed.Role('Drive', { 
+        have : {
+            driving : false
+        },
+        
+        methods : {
+            drive : function (where) { this.driving = true },
+            stop : function () { this.driving = false }
+        },
+        
+        override : {
+            walk : function (where) { 
+                this.drive(where); 
+            }
+        }
+    }).c;    
+    
+    
+    var Vegetarian = new Joose.Managed.Role('Vegetarian', { 
+        override : {
+            eat : function (food) { 
+                if (!food.meta.hasMethod('walk')) this.SUPER(food); 
+            }
+        }
+    }).c;    
+
+    
+    var Human = new Joose.Managed.Class('Human', {
+        
+        isa : Creature,
+        
+        does : [ Drive, Vegetarian ],
+        
+        have : {
+            cleanHands : false
+        },
+        
+        methods : {
+            washHands : function(){
+                this.cleanHands = true;
+            }
+        },
+        
+        before : {
+            eat : function () { this.washHands() }
+        },
+        
+        after : {
+            stopEat : function () { this.cleanHands = false; }
+        }
+        
+    }).c;
+    
+    
+    var human = new Human();
+    
+    human.eat(cannibal1);
+    this.ok(!human.eating, "Human doesn't eat thing which can 'walk' #1");
+    this.ok(!human.cleanHands, "Human have not washed hands");
+    
+    human.eat(creature);
+    this.ok(!human.eating, "Human doesn't eat thing which can 'walk' #2");
+    
+    human.eat(plant);
+    this.ok(human.eating, "Human is vegetarian");
+    this.ok(human.cleanHands, "Human washed hands before eating");
+    
+    human.stopEat();
+    this.ok(!human.cleanHands, "Human has dirty hands after he ate");
+    
+    human.walk('supermarket');
+    this.ok(!human.walking, "Humans mostly drives #1");
+    this.ok(human.driving, "Humans mostly drives #2");
+    
+    human.eat(plant);
+    this.ok(human.eating, "Human can eat during driving");
+    human.stopEat();
+    
+    human.stop();
+    this.ok(!human.driving, "Humans stopped");
+    
+    
+    //==================================================================================================================================================================================
+    this.diag("Mutability");
+    
+    Drive.meta.extend({ 
+        override : {
+            eat : function (food) { 
+                if (!this.driving) this.SUPER(food); 
+            }
+        }
+    });      
+    
+    human.drive('supermarket');
+    this.ok(human.driving, "Humans is driving");
+    
+    human.eat(plant);
+    this.ok(!human.eating, "Human now cant eat during driving");
+    
+    human.stop();
+    
+    human.eat(plant);
+    this.ok(human.eating, "Human now can eat again");
+    human.stopEat();
+    
+
+    
+    Cannibalism.meta.extend({ 
+        does : [ Vegetarian ]
+    });
+    
+    cannibal1.eat(creature);
+    this.ok(!cannibal1.eating, "Cannibal eats only creatures from his species which cant walk now");
+
+    cannibal1.eat(cannibal2);
+    this.ok(!cannibal1.eating, "Cannibal eats only creatures from his species which cant walk now");
+    
+    cannibal1.eat(plant);
+    this.ok(!cannibal1.eating, "Cannibal eats only creatures from his species which cant walk now");
+    
+
+    Cannibalism.meta.extend({ 
+        doesnt : [ Vegetarian ]
+    });
+    
+    cannibal1.eat(cannibal2);
+    this.ok(cannibal1.eating, "Cannibal now can eat again");
+    cannibal1.stopEat();
     
 };
 
