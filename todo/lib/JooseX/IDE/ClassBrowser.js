0c68cd238f4d14400da58d54b191d8c9b60b4596
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-06-02 09:10:47 +0000

    - applied patch for JooseX.IDE.ClassBrowser (contributed by Jan Van Lysebeth)
    git-svn-id: http://joose-js.googlecode.com/svn/branches/mutability@823 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- todo/lib/JooseX/IDE/ClassBrowser.js
+++ todo/lib/JooseX/IDE/ClassBrowser.js
@@ -1,8 +1,8 @@
 Module("JooseX.IDE", function () {
     Class("ClassBrowser", {
-        classMethods: {
-            test: function () { return "I am just a test" }
-        },
+//        classMethods: {
+//            test: function () { return "I am just a test" }
+//        },
         methods: {
             
             $: function (id) {
@@ -12,9 +12,25 @@ Module("JooseX.IDE", function () {
             initialize: function () {
                 this.fillSelect(
                     this.moduleSelect(), 
-                    Joose.A.grep(Joose.Module.getAllModules(), function (m) { return !m.meta.isEmpty() }),
+                    this.getAllModules(),
                     true
                 )
+            },
+	    
+            getAllModules: function() {
+                var allModules = [];
+                function flattenNameSpaceTree(aNamespace) {
+                if (aNamespace.meta instanceof Joose.Namespace.Keeper) {
+                    allModules.push(aNamespace);
+                    Joose.O.eachSafe(aNamespace.meta.ns.properties, 
+                    function(prop, name) {
+                        flattenNameSpaceTree(prop);
+                    });
+                }
+                }
+                Joose.O.eachSafe(__global__.properties, 
+                function(property, name) {flattenNameSpaceTree(property)});
+                return allModules;
             },
             
             getNames: function (array) {
@@ -32,11 +48,8 @@ Module("JooseX.IDE", function () {
                 Joose.O.each(array, function (thing) {
                     var option = document.createElement("option");
                     var name   = thing.name;
-                    if(thing.getName) {
-                        name = thing.getName()
-                    }
                     if(useMeta) {
-                        name = thing.meta.getName()
+                        name = thing.meta.name
                     }
                     option.innerHTML   = name
                     option.valueObject = thing
@@ -51,29 +64,42 @@ Module("JooseX.IDE", function () {
             
             selectedModule: function (select) {
                 var module = this.selectedValueObject(select);
-                this.fillSelect(this.classSelect(), module.meta.getElements(), true)
+                this.fillSelect(this.classSelect(), this.getAllClasses(module), true)
                 this.clearBody()
                 this.clearSelect(this.categoriesSelect())
                 this.clearSelect(this.elementsSelect())
+            },
+        
+            getAllClasses: function(aModule) {
+                var allClasses = [];
+                Joose.O.eachSafe(aModule.meta.ns.properties, 
+                    function(prop, name) {
+                        if (prop.meta instanceof Joose.Proto.Class &&
+                            !(prop.meta instanceof Joose.Namespace.Keeper)) {
+                            allClasses.push(prop);
+                        }
+                    });
+                return allClasses;
             },
             
             selectedClass: function (select) {
                 var c = this.selectedValueObject(select);
                 this.fillSelect(this.categoriesSelect(), [
                     {c: c, name: "Instance Methods"},
+                    {c: c, name: "Attributes"},
                     {c: c, name: "Class Methods"},
-                    {c: c, name: "Attributes"}
+                    {c: c, name: "Class Attributes"},
                 ])
                 
-                var html = "<p><strong>"+c.meta.className()+"</strong></p>"
+                var html = "<p><strong>"+c.meta.localName+"</strong></p>"
                 html    += "<ul>"
-                html    +=   "<li>Super Classes: "+c.meta.getSuperClasses()+"</li>"
+                html    +=   "<li>Super Classes: "+c.meta.superClass+"</li>"
                 html    +=   "<li>Roles: "+c.meta.getRoles()+"</li>"
                 html    += "</ul>"
                 
-                if(c.meta.renderHTML) {
-                    html += "<p>"+c.meta.renderHTML()+"</p>"
-                }
+//                if(c.meta.renderHTML) {
+//                    html += "<p>"+c.meta.renderHTML()+"</p>"
+//                }
                 
                this.$('JooseCBBody').innerHTML = html
             },
@@ -86,13 +112,16 @@ Module("JooseX.IDE", function () {
                 var cat = this.selectedValueObject(select);
                 var c   = cat.c
                 if(cat.name == "Instance Methods") {
-                    this.fillSelect(this.elementsSelect(), c.meta.getInstanceMethods())
+                    this.fillSelect(this.elementsSelect(), c.meta.methods)
                 }
-                if(cat.name == "Class Methods") {
-                    this.fillSelect(this.elementsSelect(), c.meta.getClassMethods())
+                if(cat.name == "Class Methods" && c.my) {
+                    this.fillSelect(this.elementsSelect(), c.my.meta.methods)
                 }
                 if(cat.name == "Attributes") {
-                    this.fillSelect(this.elementsSelect(), c.meta.getAttributes())
+                    this.fillSelect(this.elementsSelect(), c.meta.attributes)
+                }
+                if(cat.name == "Class Attributes" && c.my) {
+                    this.fillSelect(this.elementsSelect(), c.my.meta.attributes)
                 }
             },
             
@@ -100,14 +129,14 @@ Module("JooseX.IDE", function () {
                 var ele = this.selectedValueObject(select);
                 var html = "";
                 
-                html += "<p><strong>"+ele.getName()+"</strong></p>"
+                html += "<p><strong>"+ele.name+"</strong></p>"
                 
-                if(ele.meta.isa(Joose.Method)) {
-                    html += "<pre>"+ele.getBody().toString()+"</pre>"
+                if(ele instanceof Joose.Managed.Property.MethodModifier) {
+                    html += "<pre>"+ele.props.init.toString()+"</pre>"
                 } 
-                else if(ele.meta.isa(Joose.Attribute)) {
+                else if(ele instanceof Joose.Managed.Property.Attribute) {
                     html += "<ul>"
-                    Joose.O.each(ele.getProps(), function (value, name) {
+                    Joose.O.each(ele.props, function (value, name) {
                         html += "<li>"+name+": "+value+"</li>"
                     })
                     html += "</ul>"
