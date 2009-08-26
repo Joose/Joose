8e8f96a1d4718b6dfebab4050f90420f2358ec58
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-08-26 16:06:57 +0000

    - tested 'eachAll' and 'eachOwn' iterators for containable properties set
    git-svn-id: http://joose-js.googlecode.com/svn/branches/mutability@876 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- t/070_reflection.t.js
+++ t/070_reflection.t.js
@@ -1,5 +1,5 @@
 StartTest(function (t) {
-    t.plan(5)
+    t.plan(15)
     
     //==================================================================================================================================================================================
     t.diag("Reflection - getRoles")
@@ -26,10 +26,69 @@ StartTest(function (t) {
     //==================================================================================================================================================================================
     t.diag("Reflection - getMethods")
     
-    var methods = Joose.Meta.Class.meta.getMethods()
+    Class('TestClass1', {
+        has : {
+            attribute1 : null,
+            attribute2 : null
+        },
+        
+        methods : {
+            method1 : function () {},
+            
+            method2 : function () {}
+        }
     
-    t.ok(methods.haveProperty('getMethods'), "Joose.Meta.Class have 'getMethods' method")
-    t.ok(!methods.haveOwnProperty('getMethods'), "its not 'own' method though")
+    })
     
+    Class('TestClass2', {
+        isa : TestClass1,
+        
+        has : {
+            attribute3 : null,
+            attribute4 : null
+        },
+        
+        methods : {
+            method3 : function () {},
+            
+            method4 : function () {}
+        }
+    
+    })
+    
+    
+    var methods = TestClass2.meta.getMethods()
+    
+    t.ok(methods.haveProperty('method1'), "TestClass2 have 'method1' method")
+    t.ok(!methods.haveOwnProperty('method1'), "its not 'own' method though")
+    
+    var count = 0
+    methods.eachAll(function () { count++ })
+    
+    t.ok(count == 12, "TestClass2 has 12 methods (included inherited from Joose.Proto.Object)")
+
+    var count = 0
+    methods.eachOwn(function () { count++ })
+    
+    t.ok(count == 2, "TestClass2 has 2 own methods")
+
+    
+    //==================================================================================================================================================================================
+    t.diag("Reflection - getAttributes")
+
+    var attributes = TestClass2.meta.getAttributes()
+    
+    t.ok(attributes.haveProperty('attribute1'), "TestClass2 have 'attribute1' attribute")
+    t.ok(!attributes.haveOwnProperty('attribute1'), "its not 'own' attribute though")
+    
+    var count = 0
+    attributes.eachAll(function () { count++ })
+    
+    t.ok(count == 4, "TestClass2 has 4 attributes (included inherited from Joose.Proto.Object)")
+
+    var count = 0
+    attributes.eachOwn(function () { count++ })
+    
+    t.ok(count == 2, "TestClass2 has 2 own attributes")
     
 })
