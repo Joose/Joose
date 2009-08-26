5d2e08bde88769419e7cfa8b01e033c145483a2e
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-08-26 14:51:08 +0000

    - removed some lurking trailing semi-colons - implemented and tested 'does', 'getMethods', 'getAttributes' methods for metaclasses
    git-svn-id: http://joose-js.googlecode.com/svn/branches/mutability@872 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- t/070_reflection.t.js
+++ t/070_reflection.t.js
@@ -2,7 +2,7 @@ StartTest(function(t) {
     t.plan(5)
     
     //==================================================================================================================================================================================
-    t.diag("Reflection")
+    t.diag("Reflection - getRoles")
     
     var metaClassRoles = Joose.Meta.Class.meta.getRoles()
     
@@ -12,5 +12,24 @@ StartTest(function(t) {
     t.ok(Joose.A.exists(metaClassRoles, Joose.Managed.My), 'Joose.Managed.My is applied to Joose.Meta.Class')
     t.ok(Joose.A.exists(metaClassRoles, Joose.Managed.Attribute.Builder), 'Joose.Managed.Attribute.Builder is applied to Joose.Meta.Class')
     t.ok(Joose.A.exists(metaClassRoles, Joose.Meta.MetaRole), 'Joose.Meta.MetaRole is applied to Joose.Meta.Class')
+
+    
+    
+    //==================================================================================================================================================================================
+    t.diag("Reflection - does")
+    
+    t.ok(Joose.Meta.Class.meta.does(Joose.Namespace.Able), 'Joose.Meta.Class does Joose.Namespace.Able')
+    t.ok(Joose.Meta.Class.meta.does(Joose.Managed.My), 'Joose.Meta.Class does Joose.Managed.My')
+    
+    
+    
+    //==================================================================================================================================================================================
+    t.diag("Reflection - getMethods")
+    
+    var methods = Joose.Meta.Class.meta.getMethods()
+    
+    t.ok(methods.haveProperty('getMethods'), "Joose.Meta.Class have 'getMethods' method")
+    t.ok(!methods.haveOwnProperty('getMethods'), "its not 'own' method though")
+    
     
 })
