09ec7ec961da725019e6cd6d29e571fef0dc2941
nickolay8 <nickolay8@7be8dbaf-bf49-0410-bd10-bfef82906ff6>
2009-09-07 20:25:03 +0000

    - corrected 'meta.does' implementation
    git-svn-id: http://joose-js.googlecode.com/svn/branches/mutability@896 7be8dbaf-bf49-0410-bd10-bfef82906ff6

--- t/070_reflection.t.js
+++ t/070_reflection.t.js
@@ -1,25 +1,13 @@
 StartTest(function (t) {
-    t.plan(15)
-    
-    //==================================================================================================================================================================================
-    t.diag("Reflection - getRoles")
-    
-    var metaClassRoles = Joose.Meta.Class.meta.getRoles()
-    
-    t.ok(metaClassRoles.length == 4, 'Joose.Meta.Class has 4 roles applied')
-    
-    t.ok(Joose.A.exists(metaClassRoles, Joose.Namespace.Able), 'Joose.Namespace.Able is applied to Joose.Meta.Class')
-    t.ok(Joose.A.exists(metaClassRoles, Joose.Managed.My), 'Joose.Managed.My is applied to Joose.Meta.Class')
-    t.ok(Joose.A.exists(metaClassRoles, Joose.Managed.Attribute.Builder), 'Joose.Managed.Attribute.Builder is applied to Joose.Meta.Class')
-    t.ok(Joose.A.exists(metaClassRoles, Joose.Meta.MetaRole), 'Joose.Meta.MetaRole is applied to Joose.Meta.Class')
-
-    
+    t.plan(12)
     
     //==================================================================================================================================================================================
     t.diag("Reflection - does")
     
     t.ok(Joose.Meta.Class.meta.does(Joose.Namespace.Able), 'Joose.Meta.Class does Joose.Namespace.Able')
     t.ok(Joose.Meta.Class.meta.does(Joose.Managed.My), 'Joose.Meta.Class does Joose.Managed.My')
+    t.ok(Joose.Meta.Class.meta.does(Joose.Managed.Attribute.Builder), 'Joose.Meta.Class does Joose.Managed.Attribute.Builder')
+    t.ok(Joose.Meta.Class.meta.does(Joose.Meta.MetaRole), 'Joose.Meta.Class does Joose.Meta.MetaRole')
     
     
     
